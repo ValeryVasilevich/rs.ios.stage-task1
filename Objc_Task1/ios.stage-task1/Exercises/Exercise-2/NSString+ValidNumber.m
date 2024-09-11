b94112806dab4e991f999863830852dee7ef6c5d
ValeryVasilevich <>
2024-09-12 00:47:40 +0200

    Impl: exercise 2

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -1,8 +1,20 @@
 #import "NSString+ValidNumber.h"
 
 @implementation NSString (ValidNumber)
-
 - (BOOL)isValidNumber {
+    if (self.length <= 100 && self.length > 0) {
+        NSError *error = nil;
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^[0-9]+$" options:0 error:&error];
+
+        if (error) {
+            NSLog(@"Error creating regex: %@", [error localizedDescription]);
+            return NO;
+        }
+
+        NSInteger matches = [regex numberOfMatchesInString:self options:0 range:NSMakeRange(0, [self length])];
+
+        return matches > 0;
+    }
     return false;
 }
 
