8ed19cdcd2350d0b0fc75b3c6d4477429a16102e
ValeryVasilevich <>
2024-09-12 11:36:00 +0200

    impl: exercise 4

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,23 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    // If the input number is nil, return an empty array
+    if (!number) {
+        return @[];
+    }
+
+    NSString *numberString = [[number stringValue] stringByReplacingOccurrencesOfString:@"-" withString:@""];
+
+    NSMutableArray<NSString *> *charArray = [NSMutableArray arrayWithCapacity:numberString.length];
+
+    for (NSInteger i = 0; i < numberString.length; i++) {
+        NSString *charValue = [NSString stringWithFormat:@"%C", [numberString characterAtIndex:i]];
+        [charArray addObject:charValue];
+    }
+
+    NSArray<NSString *> *reversedArray = [[charArray reverseObjectEnumerator] allObjects];
+
+    return reversedArray;
 }
 
 @end
